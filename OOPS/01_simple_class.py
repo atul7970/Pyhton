8d695d5a578b19d220caa88f1260d02737ef3278
atul7970 <a6394tul@gmail.com>
2025-12-20 19:58:43 +0530

    init files

---
+++ OOPS/01_simple_class.py
@@ -0,0 +1,28 @@
+# class Castle:
+#     def __init__(self, name, location, num_towers):
+#         self.name = name
+#         self.location = location
+#         self.num_towers = num_towers
+
+#     def describe(self):
+#         return f"{self.name} is located in {self.location} and has {self.num_towers} towers."
+    
+# # Example usage:
+# my_castle = Castle("Winterfell", "The North", 7)
+# print(my_castle.describe())      
+
+
+
+
+class c:
+    pass
+
+class d:
+    pass
+
+print(type(c))
+
+e = c()
+print(type(e))
+print(type(e) is c)
+print(type(e) is d)
---
+++ OOPS/02_namespace.py
@@ -0,0 +1,13 @@
+class c:
+    origin = "India"
+
+print(c.origin)
+
+c.is_ok = True
+print(c.is_ok)
+
+#creating object of class c
+obj = c()
+print(obj.origin)
+print(obj.is_ok)
+ 
